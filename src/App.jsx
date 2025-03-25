8d4a6fc0a8169617321e080ab3c0ac06a8d24431
Lionel Aditya <adityaindomaret@gmail.com>
2025-03-25 16:29:12 +0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { FaUser, FaSearch } from "react-icons/fa";
 import Footer from "./components/footer.jsx";
 import HeroSection from "./components/herosection.jsx";
-import newSarticle from "./components/newsarticle.jsx";
 import NewsArticle from "./components/newsarticle.jsx";
 import LiveNewsSection from "./components/livenewsection.jsx";
-import LatestNewsSection from "./components/latestnewssection.jsx.jsx";
+import LatestNewsSection from "./components/latestnewssection.jsx";
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
-import NewsSection from "./components/newssection.jsx";
 
 
 const NewsPortal = () => {
